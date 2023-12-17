6600114173a03ecf2ea817b2745cb6b5c60cd4a1
Asis <asismelgarejo@gmail.com>
2023-12-17 15:07:02 -0500

    serverless-http was replaced

--- src/handler.ts
+++ src/handler.ts
@@ -1,4 +1,22 @@
-import serverlessHttp from "serverless-http"
-import app from './main';
+import { configure as serverlessExpress } from "@vendia/serverless-express";
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Context,
+} from "aws-lambda";
+import Application from "./main";
 
-export const handler = serverlessHttp(app);
+async function bootstrap(): Promise<any> {
+  const app = new Application();
+  await app.Init();
+  return serverlessExpress({
+    app: app.app,
+  });
+}
+export const handler = async (
+  event: APIGatewayProxyEvent,
+  context: Context
+): Promise<APIGatewayProxyResult> => {
+  const cachedServer = await bootstrap();
+  return cachedServer(event, context);
+};
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Express } from "express";
 import cors from "cors";
 import pkg from "body-parser";
 const { urlencoded } = pkg;
@@ -10,36 +10,27 @@ import { InitializeDB } from "./database";
 import OrderModule from "./modules/Order";
 import { PORT } from "./constants/app";
 
-const app = express();
+export default class Application {
+  app: Express;
+  constructor() {
+    const app = express();
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+    app.use(cors());
+    app.use(urlencoded({ extended: false }));
+    this.app = app;
+  }
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cors());
-app.use(urlencoded({ extended: false }));
+  async Init() {
+    const dbClient = await InitializeDB();
+    OrderModule.Init(dbClient, this.app);
 
-async function main() {
-  const dbClient = await InitializeDB();
-  OrderModule.Init(dbClient, app);
-  // app.listen(PORT, () => {
-  //   console.log("Server running on " + `localhost:${PORT}`);
-  // });
+    this.app.use("/hello", (_, res, _2) => {
+      res.status(200).json({ message: "Project Created by Asis Melgarejo" });
+    });
+
+    this.app.use((_, res, _2) => {
+      res.status(404).json({ error: "NOT FOUND" });
+    });
+  }
 }
-main()
-  .then(() => {})
-  .catch((err) => {
-    console.log(err);
-  });
-
-app.get("/hello", (_, res, _2) => {
-  res.status(200).json({ message: "OK DEV" });
-});
-
-app.get("/orders1", (req, res, _2) => {
-  res.status(200).json({ message: "NO MACNHES" });
-});
-
-app.use((_, res, _2) => {
-  res.status(404).json({ error: "NOT FOUND" });
-});
-
-export default app;
